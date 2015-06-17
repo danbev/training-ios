8a52bd94f53b340041559cbbbf9e4f922ec58d60
Daniel Bevenius <daniel.bevenius@gmail.com>
2015-06-17 09:53:33 +0200

    First stab at a table based time view

--- FHR/Controllers/RepsViewController.swift
+++ FHR/Controllers/RepsViewController.swift
@@ -15,11 +15,13 @@ import AVFoundation
 Controlls a Reps based workout
 
 */
-public class RepsViewController: BaseWorkoutController {
+public class RepsViewController: BaseWorkoutController, UITableViewDelegate, UITableViewDataSource {
 
+    let tableCell = "timeCell"
     var repsWorkout: RepsWorkout!
     @IBOutlet weak var repsLabel: UILabel!
     @IBOutlet weak var totalTime: UILabel!
+    @IBOutlet weak var tableView: UITableView!
 
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +31,27 @@ public class RepsViewController: BaseWorkoutController {
     public override func initWith(workout: Workout, restTimer: CountDownTimer?, finishDelegate: FinishDelegate) {
         super.initWith(workout, restTimer: restTimer, finishDelegate: finishDelegate)
         repsWorkout = workout as! RepsWorkout
+    }
+
+    public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    }
+
+    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(tableCell) as! UITableViewCell
+        if indexPath.row == 0 {
+            cell.textLabel!.text = "Rest time"
+            cell.textLabel!.textAlignment = NSTextAlignment.Center
+            cell.textLabel!.textColor = UIColor.whiteColor()
+        } else {
+            cell.textLabel!.text = "00:00"
+            cell.textLabel!.textAlignment = NSTextAlignment.Center
+            cell.textLabel!.textColor = UIColor.whiteColor()
+        }
+        return cell;
     }
 
 }
