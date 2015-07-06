52bf05f06ab5869997a4ba1a11d5a8b84fa98aa1
Daniel Bevenius <daniel.bevenius@gmail.com>
2015-07-06 07:57:45 +0200

    Setting AVPlayerViewController view's background to gray.

--- FHR/Controllers/InfoViewController.swift
+++ FHR/Controllers/InfoViewController.swift
@@ -45,7 +45,7 @@ public class InfoViewController: UIViewController {
                     url = NSURL.fileURLWithPath(videoUrl)
                 }
                 let videoViewController = segue.destinationViewController as! AVPlayerViewController
-                videoViewController.
+                videoViewController.view.backgroundColor = UIColor.darkGrayColor()
                 videoViewController.player = AVPlayer(URL: url)
             } else {
                 container.hidden = true
